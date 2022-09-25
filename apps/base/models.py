f8cafaaa0d2c8c06d591b3d8392aab66cb3cd595
ChrisGil <givc980909@gmail.com>
2022-09-24 22:03:43 -0500

    1. apps/base/models.py 	Modificación del campo date_updated a un campo modificable 2. apps/users/migrations/0002_alter_user_date_updated.py 	Se migra el cambio del modelo 3. Se eliminan archivos sin utilizar 	apps/message_catalog/tests.py 	apps/message_catalog/views.py 	apps/users/tests.py 	apps/users/views.py

--- apps/base/models.py
+++ apps/base/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class BaseModel(models.Model):
     date_creation = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    date_updated = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
     state = models.BooleanField(default=True)
 
     class Meta:
