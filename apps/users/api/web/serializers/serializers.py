51f973a80d4215d8ac6721ca1ced519a2a4f2044
ChrisGil <givc980909@gmail.com>
2022-09-24 23:08:04 -0500

    1. apps/users/api/web/serializers/serializers.py 	Se añade decifrado de contraseña en el serializador SerializerLoginUser 	Se crea serializador para actualizar contraseña

--- apps/users/api/web/serializers/serializers.py
+++ apps/users/api/web/serializers/serializers.py
@@ -1,14 +1,25 @@
+import datetime as dt
+from typing import ClassVar
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError
 from rest_framework import serializers
 
 from apps.users.exceptions import UserValidationException
 from apps.users.models import User
+from config.backends.authentication.cryptography_user_info import DecryptRSACryptography
 
 
 class SerializerLoginUser(serializers.Serializer):
+    _decrypt_password: ClassVar[DecryptRSACryptography] = DecryptRSACryptography
     email = serializers.CharField()
     password = serializers.CharField()
+
+    def validate_password(self, value: str) -> None | str:
+        try:
+            return self._decrypt_password(value).decrypt_data
+        except Exception as e:
+            return None
 
     def validate(self, attrs):
         attr = dict(attrs)
@@ -22,7 +33,6 @@ class SerializerLoginUser(serializers.Serializer):
 
         if not user.check_password(password):
             raise UserValidationException(2)
-
         if not user.state:
             raise UserValidationException(3)
 
@@ -32,5 +42,47 @@ class SerializerLoginUser(serializers.Serializer):
         instance: User = User.objects.get_user_instance(self.validated_data.get("email"))
         instance.is_active = True
         instance.is_new = False
+        instance.save()
+        return instance
+
+
+class SerializerUpdateUserPassword(serializers.Serializer):
+    _decrypt_password: ClassVar[DecryptRSACryptography] = DecryptRSACryptography
+    password = serializers.CharField()
+    new_password = serializers.CharField()
+
+    def validate_password(self, value: str) -> str | None:
+        try:
+            return self._decrypt_password(value).decrypt_data
+        except Exception as e:
+            return None
+
+    def validate_new_password(self, value: str) -> str | None:
+        try:
+            return self._decrypt_password(value).decrypt_data
+        except Exception as e:
+            return None
+
+    def validate(self, attrs):
+        attr = dict(attrs)
+        password = attr.get("password")
+        new_password = attr.get("new_password")
+        user: User = self.context.get("user")
+
+        if not user.check_password(password):
+            raise UserValidationException(14)
+        if not user.is_active:
+            raise UserValidationException(15)
+        if password == new_password:
+            raise UserValidationException(16)
+        if not user.state:
+            raise UserValidationException(17)
+        return attrs
+
+    def update(self, **kwargs) -> User:
+        instance: User = self.context.get("user")
+        instance.set_password(self.validated_data.get("new_password"))
+        instance.date_updated = dt.datetime.now()
+        instance.is_active = False
         instance.save()
         return instance
