622e8297e9b1377fec91ba9c766a9eaf4b07b204
ChrisGil <givc980909@gmail.com>
2022-09-24 23:04:48 -0500

    1. config/backends/rsa_keys/import_keys_cryptography.py 	Se crea clase que importa claves RSA para la libreria cryptography

---
+++ config/backends/rsa_keys/import_keys_cryptography.py
@@ -0,0 +1,29 @@
+from typing import ClassVar
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+from config.constans import PASSPHRASE, FILENAME_PUBLIC_KEY_LOCATION, FILENAME_PRIVATE_KEY_LOCATION
+
+
+class ImportKeysCryptography:
+    """ Importar llaves RSA tanto privadas como publicas utilizando cryptography """
+
+    _file_name: ClassVar[str]
+    _PASSPHRASE: ClassVar[bytes] = PASSPHRASE.encode()
+    _PUBLIC_KEY_FILE: ClassVar[bytes] = FILENAME_PUBLIC_KEY_LOCATION
+    _PRIVATE_KEY_FILE: ClassVar[bytes] = FILENAME_PRIVATE_KEY_LOCATION
+
+    @property
+    def get_private_key(self):
+        with open(self._PRIVATE_KEY_FILE, mode='rb') as key_file:
+            return serialization.load_pem_private_key(
+                key_file.read(),
+                password=self._PASSPHRASE,
+            )
+
+    @property
+    def get_public_key(self):
+        with open(self._PUBLIC_KEY_FILE, mode='rb') as key_file:
+            return serialization.load_pem_public_key(
+                key_file.read(),
+            )
