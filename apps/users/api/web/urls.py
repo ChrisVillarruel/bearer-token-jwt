fcf759b5a7e030fc000d2f04dd9f501778d9490d
ChrisGil <givc980909@gmail.com>
2022-10-30 19:45:44 -0600

    ➕ADD: Se genera una nueva funcionalidad
    Se agrega a urls el nuevo servicio API REST APIForgotPassword

--- apps/users/api/web/urls.py
+++ apps/users/api/web/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from apps.users.api.web.views.views import (
+    APILoginUser,
+    APIUserInfo,
+    APIUpdatePasswordUser,
+    APIForgotPassword
+)
 
-from apps.users.api.web.views.views import APILoginUser, APIUserInfo, APIUpdatePasswordUser
+routers = SimpleRouter()
+routers.register("v1/new-pass", APIForgotPassword, basename="forgot-password")
 
 urlpatterns = [
-    path("v1/login/", APILoginUser.as_view()),
-    path("v1/UseInf/", APIUserInfo.as_view()),
-    path("v1/UpdPassUsr/", APIUpdatePasswordUser.as_view()),
+    path("v1/edit-password/", APIUpdatePasswordUser.as_view()),
+    path("v1/sig-in/", APILoginUser.as_view()),
+    path("v1/", APIUserInfo.as_view()),
 ]
+
+urlpatterns += routers.urls
